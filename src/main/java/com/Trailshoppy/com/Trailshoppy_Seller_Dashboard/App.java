adc3bf86882fd02d8119d3fc3dc4e058433583ba
unknown <gurupreet108@gmail.com>
2024-09-04 22:52:43 +0530

    created New Trailshoppy project

---
+++ src/main/java/com/Trailshoppy/com/Trailshoppy_Seller_Dashboard/App.java
@@ -0,0 +1,13 @@
+package com.Trailshoppy.com.Trailshoppy_Seller_Dashboard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/Trailshoppy/com/Trailshoppy_Seller_Dashboard/hello.java
@@ -0,0 +1,10 @@
+package com.Trailshoppy.com.Trailshoppy_Seller_Dashboard;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello there");
+
+	}
+
+}
