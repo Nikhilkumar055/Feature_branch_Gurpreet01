be70b3164b344d00db20d3e0912c3075a69c7900
unknown <gurupreet108@gmail.com>
2024-09-04 23:00:34 +0530

    changes made

--- src/main/java/com/Trailshoppy/com/Trailshoppy_Seller_Dashboard/hello.java
+++ src/main/java/com/Trailshoppy/com/Trailshoppy_Seller_Dashboard/hello.java
@@ -3,7 +3,7 @@ package com.Trailshoppy.com.Trailshoppy_Seller_Dashboard;
 public class hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello there");
+		System.out.println("hello there!");
 
 	}
 
